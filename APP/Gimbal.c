7a7e19aa983ac2544823f0fb316f4e8010f8698c
Magical-jejbd <178537630@qq.com>
2026-07-31 16:13:43 +0800

    超级好用的参数I

--- APP/Gimbal.c
+++ APP/Gimbal.c
@@ -54,10 +54,10 @@ extern Chassis_Move_State_e car_stop;
 
 static float slide_target_x   =  250.00f ;    /* 目标位置: 画面中心 (640/2) */
 uint32_t motor_zero_point =  0;
-#define SLIDE_SERVO_RANGE      100     /* 最大角度范围，需保证一次循环能转完 */
+#define SLIDE_SERVO_RANGE      60    /* 最大角度范围，需保证一次循环能转完 */
 #define SLIDE_VEL_LPF_ALPHA    0.3f    /* 速度低通滤波系数 */
-#define SLIDE_VEL_FF_GAIN      0.3f   /* 速度前馈增益 */
-#define SLIDE_X_LPF_ALPHA      0.25f   /* X坐标低通滤波系数，越小越平滑 */
+#define SLIDE_VEL_FF_GAIN      0.5f   /* 速度前馈增益 */
+#define SLIDE_X_LPF_ALPHA      0.3f   /* X坐标低通滤波系数，越小越平滑 */
 #define SLIDE_ACC_GAIN         50.00f
 
 static pid_init_config_s cfg = {   //动态pid这一块
